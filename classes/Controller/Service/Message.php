80efc20971af29ab4b03426290f71d4918e2c915
Mauricio S. Otta <mauricio@portnumber53.com>
2016-01-20 14:23:52 -0800

    Service class and task to send email messages from RabbitMQ queue

---
+++ classes/Controller/Service/Message.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Class Controller_Service_Message
+ */
+class Controller_Service_Message extends Controller_Service_Core_Message
+{
+}
+
