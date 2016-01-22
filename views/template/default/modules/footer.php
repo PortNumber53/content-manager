4d87ef7959286dab0fb0a1d67efff56817612d26
Mauricio S. Otta <mauricio@portnumber53.com>
2016-01-22 02:34:15 -0800

    Improve View class and basic modules for default template

---
+++ views/template/default/modules/footer.php
@@ -0,0 +1,14 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Created by IntelliJ IDEA.
+ * User: mauricio
+ * Date: 10/28/2015
+ * Time: 1:03 AM
+ */
+
+?>
+<nav class="navbar navbar-default navbar-fixed-bottom">
+    <div class="container">
+        Footer
+    </div>
+</nav>
---
+++ views/template/default/modules/twitter.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Created by IntelliJ IDEA.
+ * User: mauricio
+ * Date: 1/22/2016
+ * Time: 2:18 AM
+ */
+
+?>TWITTER
