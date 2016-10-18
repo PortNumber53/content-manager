f5c3264ef5baf7eb9a8f21ac4d54a385b9aea870
Mauricio Otta <mauricio@portnumber53.com>
2016-10-18 02:00:00 -0700

    Facebook base file

--- views/template/default/modules/facebook.php
+++ views/template/default/modules/facebook.php
@@ -1,9 +1,8 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-/**
- * Created by IntelliJ IDEA.
- * User: mauricio
- * Date: 1/22/2016
- * Time: 2:20 AM
- */
-
-?>FACEBOOK
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=<?php echo Arr::path($site_settings, 'facebook.app_id'); ?>";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
